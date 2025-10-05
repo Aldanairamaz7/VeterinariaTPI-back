621cf04078e1ade8aa9bd16af0753308fc8e1c84
Cristian Salinas <salinas.cristian15@gmail.com>
2025-10-05 16:36:50 -0300

    added pet.services

---
+++ src/Services/pet.service.js
@@ -0,0 +1,69 @@
+import { Pet } from "../entities/Pet";
+import { User } from "../entities/User"
+
+
+/* Testear */
+export const editPet = async (req, res) =>{
+
+try
+    {
+        const {id, name, age, breed, imageURL} = req.body
+        if (!id){
+            return res.status(400).json({message: 'ID de la mascota es requerido'})
+        }
+        const user = await User.findByPk(req.user.id)
+        if(!user){
+            return res.status(404).json({message: 'Usuario no encontrado'})
+        }
+        const pet = await Pet.findOne({
+            where:{
+                id,
+                userId: user.id
+            }
+        })
+        if(!pet){
+            return res.status(404).json({message: 'Mascota no encontrada'})
+        }
+        await Pet.update({
+            name,
+            age,
+            breed,
+            imageURL
+        })
+        return res.status(200).json({ pet })
+    }catch(error){
+        console.error('Error al modificar la mascota', error)
+        res.status(500).json({message:'Error interno del servidor'})
+    }
+
+}
+
+/* export const removePet = async (req, res) =>{
+    try{
+        const {name, age, breed, imageURL } = req.body
+
+        const user = await User.findByPk(req.user.id, {
+                    attributes: ["id", "firstName", "lastName", "dni", "email", "password"],
+                    include:[
+                        {
+                            model: Pet,
+                            as:'pets'
+                        }
+                    ]
+                });
+        
+        if (user){
+            const deletedPet = Pet.drop({
+                id,
+                name,
+                age,
+                breed,
+                imageURL,
+                userId: user.id
+            })
+        }
+
+        return res.status(200).json({ deletedPet })
+
+    }
+} */
